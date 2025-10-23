082090153e8d811ca0ac37ac694303040563c3e5
Gabriele <gabrieleputignano212@gmail.com>
2025-10-23 12:35:07 +0200

    about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,40 @@
 import useDocumentTitle from "./hooks/useDocumentTitle";
-import {Typography} from "@mui/material";
-function About(){
+import {Typography, Box, Grid} from "@mui/material";
+function About() {
     useDocumentTitle("About")
     return (
-        <Typography variant="p">Qui diciamo chi siamo e parliamo del progetto, a cosa serve, a chi è rivolto, ecc... </Typography>
+        <Box>
+            <Grid container spacing={1}>
+                <Grid xs={12} sm={6} md={4} lg={3} size={6} sx={{textAlign: "center"}}>
+                    <Typography sx={{textAlign: "center", color: "#cad2c5", fontSize: "80px", marginBottom: "30px",fontWeight:"bold"}}>About Us</Typography>
+                    <img src={"https://www.clipartmax.com/png/middle/35-355812_about-us-icon-icon.png"}
+                         alt="Immagine About Us"
+                         style={{width: "50%", height: "auto%", borderRadius: "50%", overFlow:"hidden"}}/>
+                </Grid>
+                <Grid xs={12} sm={6} md={4} lg={3} size={4}>
+                    <h1 style={{textAlign: "center"}}>Chi Siamo: La tua vita in Film</h1>
+                    <Typography sx={{textAlign: "center", color: "#cad2c5", fontSize: "20px"}}>
+                        È un progetto realizzato per tutti gli appassionati del mondo cinematografico. <br/>
+                        Per questo abbiamo creato un luogo digitale dove l'amore per il cinema prende forma. Utilizzalo
+                        come diario personale per registrare e condividere le tue opinioni
+                        personali sui film che guardi o semplicemente per tenere traccia dei film che hai visto in
+                        passato. <br/>
+                        Immediato, Personale e Sociale.
+                    </Typography>
+                    <h1 style={{textAlign: "center", marginTop: "50px"}}>Cosa offre</h1>
+                    <ul style={{textAlign: "center", color: "#cad2c5", fontSize: "20px"}}>
+                        <li>Tracciamento completo: tieni traccia dei film che hai visto e di quelli che hai visto in
+                            passato e aggiungi alla tua lista dei desideri quelli che vorresti vedere;
+                        </li>
+                        <li>Scoperta Autentica: trova il tuo prossimo film fidandoti dei gusti della community e scopri
+                            i loro film preferiti sulla loro pagina profilo;
+                        </li>
+                        <li>Recensisci e Condividi: pubblica le tue recensioni dettagliate in uno spazio positivo</li>
+                    </ul>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
+
 export default About;
